48a98a5288b19d503d8f3306a5bd23fb6b4b7874
LPredmore <124374222+LPredmore@users.noreply.github.com>
2025-06-05 12:16:19 -0500

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -144,18 +144,21 @@ export type Database = {
           created_at: string
           id: string
           title: string
+          start_time: string | null
           user_id: string | null
         }
         Insert: {
           created_at?: string
           id?: string
           title: string
+          start_time?: string | null
           user_id?: string | null
         }
         Update: {
           created_at?: string
           id?: string
           title?: string
+          start_time?: string | null
           user_id?: string | null
         }
         Relationships: [
